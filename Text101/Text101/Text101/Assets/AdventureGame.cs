f4ac02e19830d02eb52a0757da58bfd9ef74e36f
Duray <42286103+exian1992@users.noreply.github.com>
2018-08-11 10:05:59 +0700

    Initial commit

---
+++ Text101/Text101/Text101/Assets/AdventureGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+	// Use this for initialization
+	void Start () {
+        textComponent.text = "Where am I? Am I dead? Not yet... I think.";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
